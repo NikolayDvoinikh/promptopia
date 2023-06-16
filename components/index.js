c9c731e0c5ced6670c32f4179cc0170dad486e08
NikolayDvoinikh <nikolaydvoinikh@gmail.com>
2023-06-16 23:38:43 +0300

    init project

---
+++ components/index.js
@@ -0,0 +1,8 @@
+import Feed from "./Feed";
+import Nav from "./Nav";
+import PromptCard from "./PromptCard";
+import Profile from "./Profile";
+import Form from "./Form";
+import Provider from "./Provider";
+
+export { Feed, Nav, PromptCard, Profile, Form, Provider };
